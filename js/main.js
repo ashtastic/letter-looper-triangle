dab10530c29f24fea05709ea7d2f745bbb64718c
ashtastic <amurr20@gmail.com>
2016-02-23 17:36:36 -0500

    Completed assignment.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var enterLetter = prompt('Enter a letter:');
+
+var i;
+
+var j;
+
+for (i = 1; i < 11; i++) {
+
+for (j = 0; j < i; j++) {
+   document.write(enterLetter)+ '<br>';
+}
+   document.write('<br>');
+
+}
+
+
